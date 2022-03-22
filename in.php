1d848e5a2f0fb7f60beebd0b246231404afa0c47
jackartaplus <101051386+jackartaplus@users.noreply.github.com>
2022-03-22 11:23:43 +0200

    fix

--- in.php
+++ in.php
@@ -4,13 +4,13 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$client  = @$_SERVER['HTTP_CLIENT_IP'];
+//$client  = @$_SERVER['HTTP_CLIENT_IP'];
 $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
 $remote  = @$_SERVER['REMOTE_ADDR'];
 
-if(filter_var($client, FILTER_VALIDATE_IP))
-    $ip = $client;
-elseif(filter_var($forward, FILTER_VALIDATE_IP))
+//if(filter_var($client, FILTER_VALIDATE_IP))
+//    $ip = $client;
+if(filter_var($forward, FILTER_VALIDATE_IP))
     $ip = $forward;
 else
     $ip = $remote;
